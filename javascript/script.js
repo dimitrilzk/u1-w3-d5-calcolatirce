9f349bce8bba4cf9aaccc7cbb307526a36b878f7
dimitrilzk <dimitrilazuka95@gmail.com>
2022-09-16 10:30:29 +0200

    script

--- javascript/script.js
+++ javascript/script.js
@@ -2,9 +2,10 @@ function writeToDisplay(element) {
   document.getElementById("display").value += element;
 }
 function showResult() {
-  let result = eval(document.getElementById("content-display").value);
+  let result = eval(document.getElementById("display").value);
   document.getElementById("display").value = result;
 }
 function deleteDisplay() {
-  document.getElementById("display").value;
+  document.getElementById("display").placeholder = "0";
+  document.getElementById("display").value = "";
 }
